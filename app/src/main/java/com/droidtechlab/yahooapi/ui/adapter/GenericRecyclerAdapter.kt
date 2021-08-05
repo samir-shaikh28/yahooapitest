04e13139548499f1e7d3f544d2e0faba4e333cf2
samir_shaikh <sksamir09@gmail.com>
2021-08-05 14:04:00 +0530

    Generic recycler adapter

---
+++ app/src/main/java/com/droidtechlab/yahooapi/ui/adapter/GenericRecyclerAdapter.kt
@@ -0,0 +1,39 @@
+package com.droidtechlab.yahooapi.ui.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.recyclerview.widget.RecyclerView
+import com.droidtechlab.yahooapi.ui.listitem.AbstractListItem
+
+class GenericRecyclerAdapter(private val listCustomModel: List<AbstractListItem>) :
+    RecyclerView.Adapter<GenericRecyclerAdapter.ViewHolder>() {
+
+    class ViewHolder(private val bind: ViewDataBinding) : RecyclerView.ViewHolder(bind.root) {
+        fun getBinding(): ViewDataBinding {
+            return bind
+        }
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return listCustomModel[position].getLayoutIdentifier()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val bind = DataBindingUtil.inflate<ViewDataBinding>(
+            LayoutInflater.from(parent.context), viewType, parent, false
+        )
+        return ViewHolder(bind)
+    }
+
+    override fun getItemCount(): Int {
+        return listCustomModel.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.getBinding().setVariable(BR.group, listCustomModel[position])
+        holder.getBinding().executePendingBindings()
+    }
+
+}
